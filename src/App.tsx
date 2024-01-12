54f3883d9e22cca48482de7d7c81c444646fb7f1
Mykola Lazarchuk <nikolay.lazarchuk@gmail.com>
2024-01-12 16:58:33 +0200

    Settings for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { FirstComponent } from './components/FirstComponent'
+
 function App() {
   return (
     <div>
-      <h1>{'Hello world!'}</h1>
+      <FirstComponent />
     </div>
   )
 }
---
+++ src/components/FirstComponent/index.jsx
@@ -0,0 +1,7 @@
+import './style.css'
+export const FirstComponent = () => (
+  <div>
+    <h1 style={{ color: 'red' }}>{'Hello World!'}</h1>
+    <p className={'text'}>{'I did it!'}</p>
+  </div>
+)
