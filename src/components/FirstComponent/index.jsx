e74fc62515b0cc3aa8a13e642183592ae2556111
Mykola Lazarchuk <nikolay.lazarchuk@gmail.com>
2024-01-16 10:33:05 +0200

    Add folder images

--- src/components/FirstComponent/index.jsx
+++ src/components/FirstComponent/index.jsx
@@ -1,7 +1,9 @@
+import myFoto from '../../images/2.jpg'
 import './style.css'
 export const FirstComponent = () => (
   <div>
     <h1 style={{ color: 'red' }}>{'Hello World!'}</h1>
     <p className={'text'}>{'I did it!'}</p>
+    <img src={myFoto} alt={'myFoto'} />
   </div>
 )
