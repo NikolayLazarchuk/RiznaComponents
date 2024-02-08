563b84be4ee5397d3d26bb87c5631c4b6315428b
Mykola Lazarchuk <nikolay.lazarchuk@gmail.com>
2024-02-08 21:01:46 +0200

    Fix ProjectCardLink

--- src/components/ProjectsCardLink/index.tsx
+++ src/components/ProjectsCardLink/index.tsx
@@ -2,32 +2,34 @@ import { FC } from 'react'
 import styled from 'styled-components'
 
 export const ProjectsCardLink: FC = () => (
-  <StylePCL>
-    <a href={'https://example.com'} className={'projects-card-link'}>
-      <div className={'projects-card-link-text'}>{'UX/UI'}</div>
-    </a>
-  </StylePCL>
+  <StyleDivPCL>
+    <StyleAPCL href={'https://example.com'}>
+      <StylePPCL>{'UX/UI'}</StylePPCL>
+    </StyleAPCL>
+  </StyleDivPCL>
 )
 
-const StylePCL = styled.div`
-  .projects-card-link {
-    display: flex;
-    padding-top: 0.35vw;
-    padding-right: 1.53vw;
-    padding-bottom: 0.35vw;
-    padding-left: 1.53vw;
-    border-top-left-radius: 6.9vw;
-    border-top-right-radius: 6.9vw;
-    border-bottom-left-radius: 6.9vw;
-    border-bottom-right-radius: 6.9vw;
-    background-color: #defd7e;
-    text-decoration: none;
-  }
-  .projects-card-link-text {
-    font-family: Neuehaasdisplay;
-    color: #27252b;
-    font-size: 1vw;
-    line-height: 1.4vw;
-    font-weight: 700;
-  }
+const StyleAPCL = styled.a`
+  text-decoration: none;
+  padding-top: 0.35vw;
+  padding-right: 1.53vw;
+  padding-bottom: 0.35vw;
+  padding-left: 1.53vw;
+`
+const StyleDivPCL = styled.div`
+  display: flex;
+  /* padding-top: 0.35vw;
+  padding-right: 1.53vw;
+  padding-bottom: 0.35vw;
+  padding-left: 1.53vw; */
+  border-radius: 6.9vw;
+  background-color: #defd7e;
+`
+const StylePPCL = styled.p`
+  font-family: Neuehaasdisplay;
+  color: #27252b;
+  font-size: 1vw;
+  line-height: 1.4vw;
+  font-weight: 700;
+  margin: 0; 
 `
